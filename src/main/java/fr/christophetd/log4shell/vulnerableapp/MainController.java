cecdec89537f133d7392f8ed6b530d3d03f07ca4
manjula-aw <manjula.aw@gmail.com>
2021-12-16 14:58:10 +0800

    added json based testing and printf testing

--- src/main/java/fr/christophetd/log4shell/vulnerableapp/MainController.java
+++ src/main/java/fr/christophetd/log4shell/vulnerableapp/MainController.java
@@ -7,16 +7,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Level;
 
 @RestController
 public class MainController {
 
-    private static final Logger logger = LogManager.getLogger("HelloWorld");
+    private static final Logger logger = LogManager.getLogger("MainController");
 
     @GetMapping("/")
     public String index(@RequestHeader("X-Api-Version") String apiVersion) {
         logger.info("Received a request for API version " + apiVersion);
+        logger.printf(Level.INFO, "Using Printf: %s", apiVersion);
         return "Hello, world!";
     }
 
+    @PostMapping(path= "/", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Object> addEmployee(@RequestBody Employee employee) 
+    {
+        logger.info("Info: employee is saved: " + employee);
+        return ResponseEntity.ok().build();
+    }
 }
